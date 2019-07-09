b2d422f396d24ec9d521b2fb01c44a60d5f2be13
Marcel Goldstein <marcel.goldstein@outlook.com>
2019-07-10 00:08:01 +0200

    make course limit configurable

--- MiniatureGolf.DAL/Migrations/20190709212208_AddCourseHitLimit.Designer.cs
+++ MiniatureGolf.DAL/Migrations/20190709212208_AddCourseHitLimit.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MiniatureGolf.DAL;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -9,9 +10,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MiniatureGolf.DAL.Migrations
 {
     [DbContext(typeof(MiniatureGolfContext))]
-    partial class MiniatureGolfContextModelSnapshot : ModelSnapshot
+    [Migration("20190709212208_AddCourseHitLimit")]
+    partial class AddCourseHitLimit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MiniatureGolf.DAL/Migrations/20190709212208_AddCourseHitLimit.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MiniatureGolf.DAL.Migrations
+{
+    public partial class AddCourseHitLimit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CourseHitLimit",
+                table: "Games",
+                nullable: false,
+                defaultValue: 7);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CourseHitLimit",
+                table: "Games");
+        }
+    }
+}
