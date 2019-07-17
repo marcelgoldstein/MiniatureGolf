ab7d953724d093c9a5da499ea7830b44cb8bf85a
Marcel Goldstein <marcel.goldstein@outlook.com>
2019-07-18 01:21:50 +0200

    - change navMenu-color - currentpage is now displayed in NavMenu instead of h1 on every page (more space for content) - changed URIs of pages from "miniaturaGolf" to "scoreboard" and "gamesList" to "scores" - on "scores"-page set width of last column to 5000px so long...long content can now be displayed with an horizontal scrollbar instead of beeing pressed into 20 px (on small displays). additionally all row now have the same height.

---
+++ MiniatureGolf/Shared/NavMenu.razor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MiniatureGolf.Shared
+{
+    public class NavMenuModel : ComponentBase
+    {
+
+        protected bool collapseNavMenu = true;
+
+        protected string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+        protected void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+
+        [Inject] protected IUriHelper uriHelper { get; set; }
+
+        protected string CurrentPageName { get; set; }
+
+        protected List<(string href, string name)> Pages { get; private set; } = new List<(string href, string name)>() { ("scoreboard", "Scoreboard" ), ( "scores", "Scores" ) };
+
+        protected override void OnInit()
+        {
+            base.OnInit();
+
+            this.uriHelper.OnLocationChanged += this.UriHelper_OnLocationChanged;
+            this.RefreshCurrentPageLink();
+        }
+
+        private void UriHelper_OnLocationChanged(object sender, Microsoft.AspNetCore.Components.Routing.LocationChangedEventArgs e)
+        {
+            this.RefreshCurrentPageLink();
+        }
+
+        private void RefreshCurrentPageLink()
+        {
+            var relativeUri = uriHelper.ToBaseRelativePath(uriHelper.GetBaseUri(), this.uriHelper.GetAbsoluteUri());
+            var pageEntry = this.Pages.SingleOrDefault(a => relativeUri.ToUpper().StartsWith(a.href.ToUpper()));
+            pageEntry = (pageEntry == default ? this.Pages[0] : pageEntry);
+            this.CurrentPageName = pageEntry.name;
+
+            this.StateHasChanged();
+        }
+    }
+}
